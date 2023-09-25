a3054e180000b888f677c627ead36b866fbceb29
yriyazi <iyasiniyasin98@gmail.com>
2023-09-25 03:58:27 -0500

    Update

--- simulate.py
+++ simulate.py
@@ -1,1 +1,11 @@
-pass
+import pybullet as p
+import time 
+
+physicsClient = p.connect(p.GUI)
+# p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
+
+for i in range(1000):
+    print(i)
+    p.stepSimulation()
+    time.sleep(1/60)
+p.disconnect()
