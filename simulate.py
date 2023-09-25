7ac070a1707d8962931e1affd9d7590ae883e085
yriyazi <iyasiniyasin98@gmail.com>
2023-09-25 06:01:04 -0500

    Updates

--- simulate.py
+++ simulate.py
@@ -1,1 +1,12 @@
 
+import pybullet as p
+import time 
+
+physicsClient = p.connect(p.GUI)
+# p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
+p.loadSDF("box.sdf")
+for i in range(1000):
+    print(i)
+    p.stepSimulation()
+    time.sleep(1/60)
+p.disconnect()
